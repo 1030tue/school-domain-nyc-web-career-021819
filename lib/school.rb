bd1e92832a8fcebc352554ae536db3f04c2d21db
Kate Kim <katekim5809@gmail.com>
2019-02-20 23:49:13 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+
+def add_student(name, grade)
+  (@roster[grade] ||=[]) <<name
+end
+
+def grade(num)
+  @roster[num]
+end
+
+def sort
+  sorted={}
+  @roster.each do |name,grade|
+    sorted[grade.sort] = name
+end
+sorted.sort_by {|key, val| key}.to_h.invert
+end
+
+end
